261b1a04b7f0069fa6c2810db81bda7ccd4c9082
StickzCoder <109220460+StickzCoder@users.noreply.github.com>
2022-10-05 02:35:25 -0700

    Add files via upload

--- client/cl_main.lua
+++ client/cl_main.lua
@@ -55,7 +55,7 @@ function refreshPlayerWhitelisted()
 	if not PlayerData then return false end
 	if not PlayerData.job then return false end
 	if Config.Debug then return true end
-	for k,v in ipairs({'police'}) do
+	for k,v in ipairs({'police', 'sheriff', 'lspd', 'lscs', 'sahp', 'bcso'}) do
 		if v == PlayerData.job.name then
 			return true
 		end
